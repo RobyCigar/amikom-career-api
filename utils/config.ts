7b445255fcf6f92ec4cd48507c5628b6c5caa31f
roby <rabihutomo11@gmail.com>
2021-11-08 16:10:58 +0700

    fix: typo di config anjir

--- utils/config.ts
+++ utils/config.ts
@@ -2,7 +2,7 @@ const config: any = {
   BASE: "https://career.amikom.ac.id/",
   BASEAPI:
     process.env.NODE_ENV === "production"
-      ? "https://amikom-carrer-api.vercel.app/api"
+      ? "https://amikom-career-api.vercel.app/api"
       : "http://localhost:3000/api",
 };
 
